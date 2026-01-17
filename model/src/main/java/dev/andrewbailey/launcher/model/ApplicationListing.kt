b605d1dcfcb812fc2f0a23f0eb65ad4db4ba96b1
Andrew Bailey <me@andrewbailey.dev>
2026-01-17 18:41:05 -0500

    Fix unexpected backstacks in launched apps

--- model/src/main/java/dev/andrewbailey/launcher/model/ApplicationListing.kt
+++ model/src/main/java/dev/andrewbailey/launcher/model/ApplicationListing.kt
@@ -13,4 +13,5 @@ fun ApplicationListing.toIntent() = Intent().apply {
     component = toComponentName()
     action = Intent.ACTION_MAIN
     addCategory(Intent.CATEGORY_LAUNCHER)
+    setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
 }
